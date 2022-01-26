bcd1a8e2509edbebc1392369d99008f265fb3cf8
Monica <garciavegamonika@gmail.com>
2022-01-26 17:03:12 -0500

    agregar metodos faltantes

--- src/main/java/io/monica/userapi/controllers/UserController.java
+++ src/main/java/io/monica/userapi/controllers/UserController.java
@@ -3,12 +3,11 @@ package io.monica.userapi.controllers;
 import io.monica.userapi.models.User;
 import io.monica.userapi.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@RequestMapping
 @AllArgsConstructor
 @RestController
 public class UserController {
@@ -16,8 +15,27 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users/{id}")
-    public Optional<User> findUserById(@PathVariable String id){
+    public Optional<User> findUserById(@PathVariable String id) {
         return userService.findById(id);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable String id){
+        userService.deleteById(id);
+    }
+    @GetMapping("/users")
+    public Iterable<User> findAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping("/users")
+    public User saveUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user){
+        return userService.edit(user);
     }
 
 }
